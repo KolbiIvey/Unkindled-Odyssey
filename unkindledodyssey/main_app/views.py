4550a8e5ce84a554c0c94ee61e5043133467f323
KolbiIvey <kolbiivey@gmail.com>
2023-05-08 11:17:03 -0600

    almost done with auth, i think

--- unkindledodyssey/main_app/views.py
+++ unkindledodyssey/main_app/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from .models import Character
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+
+
+
+
+
+
+def home(request):
+  return render(request, 'home.html')
+
+
+def about(request):
+  return render(request, 'about.html')
+
+def characters_index(request):
+  characters = Character.objects.all()
+  return render(request, 'characters/index.html',{
+    'characters': characters
+  })
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
