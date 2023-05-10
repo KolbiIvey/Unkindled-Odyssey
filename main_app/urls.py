ca4ba6a66a6aa08ed5d0d50f6ff98c7fdeecd817
KolbiIvey <kolbiivey@gmail.com>
2023-05-10 11:44:59 -0600

    fixed api key error thanks to instructional team, added full crud on character model

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('characters/<int:character_id>/story/', views.character_story, name='character_story'),
     path('accounts/signup/', views.signup, name='signup'),
     path('characters/create/', views.CharacterCreate.as_view(), name='characters_create'),
+    path('characters/<int:pk>/update/', views.CharacterUpdate.as_view(), name='character_update'),
+    path('characters/<int:pk>/delete/', views.CharacterDelete.as_view(), name='character_delete'),
+
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Character
 from .forms import CharacterForm
 from django.contrib.auth import login
@@ -12,9 +12,7 @@ import os
 
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
-
-
+API_KEY = os.environ['API_KEY']
 
 
 def home(request):
@@ -32,28 +30,24 @@ def characters_index(request):
     'characters': characters
   })
 
+
 def characters_detail(request, character_id):
   character = Character.objects.get(id=character_id)
   return render(request, 'characters/detail.html', {
     'character': character
   })
-  
+
 
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in via code
       login(request, user)
       return redirect('index')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
@@ -67,32 +61,43 @@ class CharacterCreate(CreateView):
   def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-  
+
+
+class CharacterUpdate(UpdateView):
+    model = Character
+    form_class = CharacterForm
+    template_name = 'main_app/character_update_form.html'
+    success_url = '/characters'
+
+
+class CharacterDelete(DeleteView):
+    model = Character
+    success_url = '/characters'
+
+
 
 def character_story(request, character_id):
     character = Character.objects.get(id=character_id)
 
+  
     openai.api_key = API_KEY
     
-    #generate story using open ai, prompt should be something about dark souls universe
-    prompt = f"Generate a short story about {character.name} in any of the dark souls games in less than 500 characters"
+    prompt = f"Generate a short story about {character.name} in the Dark Souls universe, where they fight bosses, fight invaders, and link the first flame, in less than 500 characters"
     response = openai.Completion.create(
        engine="text-davinci-002",
        prompt=prompt,
-       max_tokens=200,
+       max_tokens=500,
        n=1,
        stop=None,
        temperature=0.7,
     )
 
-    #grabbing the story from the open ai response
     story = response.choices[0].text.strip()
 
     return render(request, 'characters/detail.html', {
        'character': character,
        'story': story
     })
-
 
 
     
